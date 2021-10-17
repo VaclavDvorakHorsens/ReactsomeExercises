1f7604f13dd2f8bf01430b35b7a7ca1715f0e6ce
VaclavDvorak <vasekdvorak99@gmail.com>
2021-10-17 11:29:52 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import forecast from "./pages/Forecast";
+import Home from "./pages/home";
+import Login from "./Components/Auth/Login";
+import NavBar from "./Components/common/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <NavBar />
+          <Switch>
+              <Route path="/forecast" exact component={forecast} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/" exact component={Home} />
+          </Switch>
+      </>
   );
 }
-
 export default App;
+
+
+
